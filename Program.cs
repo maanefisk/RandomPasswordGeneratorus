4db9fec1713896b8f15044575c5e91f64c3ab11d
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-08-12 14:55:18 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace RandomPasswordGeneratorus
     {
         static void Main()
         {
-            Console.WriteLine("PasswordGenerator \n Options:  -l = lower case letter \n - L = upper case letter \n - d = digit \n - s = special character(!\"#¤%&/(){}[] \n Example: PasswordGenerator 14 lLssdd \n Min. 1 lower case \n Min. 1 upper case \n Min. 2 special characters \n Min. 2 digits");
+            
+            string s = "noe";
+            while (!string.IsNullOrWhiteSpace(s))
+            {
+                s = Console.ReadLine();
+                Console.WriteLine("PasswordGenerator \n Options: \n -l = lower case letter \n - L = upper case letter \n - d = digit \n - s = special character(!\"#¤%&/(){}[] \n Example: PasswordGenerator 14 lLssdd \n Min. 1 lower case \n Min. 1 upper case \n Min. 2 special characters \n Min. 2 digits");
+
+                foreach (var c in s)
+                {
+                    
+                }
+
+                if (s.Length == 2) {
+                }
+
+                if (s.Contains(int))
+            }
         }
     }
 }
