98cc910595c903f652c4763d08eb84dd5892fdd1
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-08-12 10:42:51 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RandomPasswordGeneratorus
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("PasswordGenerator \n Options:  -l = lower case letter \n - L = upper case letter \n - d = digit \n - s = special character(!\"#¤%&/(){}[] \n Example: PasswordGenerator 14 lLssdd \n Min. 1 lower case \n Min. 1 upper case \n Min. 2 special characters \n Min. 2 digits");
+        }
+    }
+}
