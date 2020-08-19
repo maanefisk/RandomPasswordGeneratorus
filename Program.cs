7c02d203a305948c1fad714ba322dff9a0c8ebd1
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-08-19 09:22:38 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Xml.Schema;
 
 namespace RandomPasswordGeneratorus
 {
     class Program
     {
+        static readonly Random Random = new Random();
         static void Main( string[] args)
         {
             if (!IsValid(args))
@@ -15,13 +17,100 @@ namespace RandomPasswordGeneratorus
             var length = Convert.ToInt32(args[0]);
             var options = args[1];
 
-            var alltogether = options.PadLeft(length, 's');
+            var alltogether = options.PadRight(length, 'd');
             var password = string.Empty;
             while (alltogether.Length > 0)
             {
                 var randomIndex = Random.Next(0, alltogether.Length - 1);
+                var category = alltogether[randomIndex];
 
+                alltogether = alltogether.Remove(randomIndex, 1);
+                if (category == 'l') password += GetRandomLowerCaseLetter();
+                else if (category == 'L') password += GetRandomUpperCaseLetter();
+                else if (category == 'd') password += GetRandomDigit();
+                else password += GetRandomSpecialCharacter();
             }
+
+            Console.WriteLine(password);
+        }
+
+        private static char GetRandomSpecialCharacter()
+        {
+            const string allSpecialCharacters = "!\"#¤%&/()=?";
+            var index = Random.Next(0, allSpecialCharacters.Length - 1);
+            return allSpecialCharacters[index];
+        }
+
+        private static char GetRandomDigit()
+        {
+            return Random.Next(0, 9).ToString()[0];
+        }
+
+        private static char GetRandomUpperCaseLetter()
+        {
+            return GetRandomLetter('A', 'Z');
+        }
+
+        private static char GetRandomLowerCaseLetter()
+        {
+            return GetRandomLetter('a', 'z');
+        }
+
+        private static char GetRandomLetter(char min, char max)
+        {
+            return (char)Random.Next(min, max);
+        }
+
+        private static bool IsValid(string[] args)
+        {
+            if (args.Length != 2) return false;
+
+            var lengthStr = args[0];
+            var options = args[1];
+
+            if (!IsValidPattern(options)) return false;
+            return IsValidLength(lengthStr);
+        }
+
+        private static bool IsValidLength(string lengthStr)
+        {
+            foreach (var character in lengthStr)
+            {
+                if (!char.IsDigit(character))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool IsValidPattern(string options)
+        {
+            foreach (var character in options)
+            {
+                const string validCharacters = "lLsd";
+                if (!validCharacters.Contains(character))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        private static void ShowHelp()
+        {
+            Console.WriteLine("PasswordGenerator <length> <options>");
+            Console.WriteLine("  Options:");
+            Console.WriteLine("  - l = lower case letter");
+            Console.WriteLine("  - L = upper case letter");
+            Console.WriteLine("  - d = digit");
+            Console.WriteLine("  - s = special character (!\"#¤%&/(){}[]");
+            Console.WriteLine("Example: PasswordGenerator 14 lLssdd");
+            Console.WriteLine("         Min. 1 lower case");
+            Console.WriteLine("         Min. 1 upper case");
+            Console.WriteLine("         Min. 2 special characters");
+            Console.WriteLine("         Min. 2 digits");
         }
     }
 }
